cc5068bbf08d172477484bfe83d59035a98c433e
fukun96 <907491706@qq.com>
2017-08-03 19:04:44 +0800

    Initial commit

---
+++ src/main/java/cn/tangcoo/tcmp056/test.java
@@ -0,0 +1,15 @@
+package cn.tangcoo.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by fukun on 2017/8/3.
+ */
+public class test {
+    @Test
+    public void test(){
+
+        System.out.println("hello world!");
+
+    }
+}
